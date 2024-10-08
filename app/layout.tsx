66e464378c4317e5c30e8e432a00558ecb3d12f8
Nick <81262895+nlicari1@users.noreply.github.com>
2024-10-08 13:40:43 -0400

    creating env

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import {Figtree} from 'next/font/google'
+import "./globals.css";
+
+const font = Figtree({subsets: ['latin']})
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "Listen to Music",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${font.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
